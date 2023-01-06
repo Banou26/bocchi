30cf320edc09bfad738f33316d11706c3d3b023e
Banou <me@banou.dev>
2023-01-06 04:51:30 +0100

    feat: ✨add npm search response type

--- src/test.tsx
+++ src/test.tsx
@@ -50,6 +50,43 @@ const firstPackages =
     .fill(0)
     .map(makeFakePackage)
 
+type Response = {
+  objects: {
+    package: {
+      name: string
+      version: string
+      description: string
+      keywords: string[]
+      date: string
+      links: {
+        npm: string
+        homepage: string
+        repository: string
+        bugs: string
+      }
+      publisher: {
+        username: string
+        email: string
+      }
+      maintainers: {
+        username: string
+        email: string
+      }[]
+    }
+    score: {
+      final: number
+      detail: {
+        quality: number
+        popularity: number
+        maintenance: number
+      }
+    }
+    searchScore: number
+  }[]
+  total: number
+  time: string
+}
+
 const { client, cache } = makeBocchi({
   typeDefs,
   resolvers: {
